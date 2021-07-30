887e50aefe852b47a39277401fd7a1e6c579d262
daito tatesawa <toida2003@gmail.com>
2021-07-30 16:20:35 +0900

    change method name in pureToF

--- pureToF.cpp
+++ pureToF.cpp
@@ -10,16 +10,16 @@ namespace CAN_Device_Lib{
         return;
       } 
       for(int i = 0;i < 4;i++){
-        ranging.buf[i] = data[i];
+        ranging_.buf[i] = data[i];
       }
     }
 
     uint16_t PureToF::ReadID()const{
-      return (1 << 10) |+ DevID;
+      return (1 << 10) |+ dev_id_;
     }
 
     int16_t PureToF::ReadRangingData()const{
-      return ranging.data;
+      return ranging_.data;
     }
     
   }
--- pureToF.hpp
+++ pureToF.hpp
@@ -14,12 +14,12 @@ namespace CAN_Device_Lib{
 
 
     class PureToF{
-        CAN_Device& dev;
-        uint16_t DevID;
+        CAN_Device& dev_;
+        uint16_t dev_id_;
 
-        byte2_convert_t ranging;
+        byte2_convert_t ranging_;
     public:
-        PureToF(CAN_Device& dev,uint16_t id):dev{dev},DevID{id}{}
+        PureToF(CAN_Device& dev,uint16_t id):dev_{dev},dev_id_{id}{}
         void RxHandler(std::vector<uint8_t> data);
 
         uint16_t ReadID()const;
