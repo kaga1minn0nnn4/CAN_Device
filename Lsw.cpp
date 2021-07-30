28c39be11afc73305e350609292dafa3971d8891
daito tatesawa <toida2003@gmail.com>
2021-07-30 16:12:03 +0900

    change method name in lsw

--- Lsw.cpp
+++ Lsw.cpp
@@ -4,7 +4,7 @@ namespace CAN_Device_Lib{
   namespace Device{
     void Lsw::Update(uint8_t p_sw_1,uint8_t p_sw_2){
       uint8_t temp = (p_sw_1 << 1) |+ p_sw_2;
-      dev.Write(DevID,&temp,1);
+      dev_.Write(dev_id_,&temp,1);
     }
   }
 }
--- Lsw.hpp
+++ Lsw.hpp
@@ -8,10 +8,10 @@ namespace CAN_Device_Lib{
     namespace Device{
 
         class Lsw{
-            CAN_Device& dev;
-            uint16_t DevID;
+            CAN_Device& dev_;
+            uint16_t dev_id_;
         public:
-            Lsw(CAN_Device& dev,uint16_t id):dev{dev},DevID{id}{}
+            Lsw(CAN_Device& dev,uint16_t id):dev_{dev},dev_id_{id}{}
             void Update(uint8_t p_sw_1,uint8_t p_sw_2);
         };
 
