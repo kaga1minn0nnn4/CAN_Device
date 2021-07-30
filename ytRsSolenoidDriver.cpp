7c0812ad80c6aeb0168d30bb84af4787fb5864b2
daito tatesawa <toida2003@gmail.com>
2021-07-30 16:30:16 +0900

    change method name in sole

--- ytRsSolenoidDriver.cpp
+++ ytRsSolenoidDriver.cpp
@@ -7,18 +7,18 @@ namespace CAN_Device_Lib{
         uint8_t ytRsSolenoidDriver::Convert(){
             uint8_t temp=0;
             for(int i=0;i<8;i++){
-                temp|=TxBuf[i]<<i;
+                temp|=tx_buf_[i]<<i;
             }
             return temp;
         }
 
         void ytRsSolenoidDriver::Set(uint8_t num,uint8_t status){
-            TxBuf[num] = status;
+            tx_buf_[num] = status;
         }
 
         void ytRsSolenoidDriver::Update(){
             uint8_t temp = Convert();
-            dev.Write(DevID,&temp,1);
+            dev_.Write(dev_id_,&temp,1);
         }
 
     }
--- ytRsSolenoidDriver.hpp
+++ ytRsSolenoidDriver.hpp
@@ -8,12 +8,12 @@ namespace CAN_Device_Lib{
     namespace Device{
 
         class ytRsSolenoidDriver{
-            CAN_Device& dev;
-            uint16_t DevID;
-            uint8_t TxBuf[8];
+            CAN_Device& dev_;
+            uint16_t dev_id_;
+            uint8_t tx_buf_[8];
             uint8_t Convert();
         public:
-            ytRsSolenoidDriver(CAN_Device& dev,uint16_t id):dev{dev},DevID{id},TxBuf{0,0,0,0,0,0,0,0}{}
+            ytRsSolenoidDriver(CAN_Device& dev,uint16_t id):dev_{dev},dev_id_{id},tx_buf_{0,0,0,0,0,0,0,0}{}
             void Set(uint8_t num,uint8_t status);
             void Update();
         };
