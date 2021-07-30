84af5d62187b64c88393c52d91cccc29727e201d
daito tatesawa <toida2003@gmail.com>
2021-07-30 16:28:44 +0900

    change method name in ws2812b

--- Ws2812b_driver.cpp
+++ Ws2812b_driver.cpp
@@ -4,7 +4,7 @@ namespace CAN_Device_Lib{
   namespace Device{
     void Ws2812b_driver::Update(uint8_t num,uint8_t r,uint8_t g,uint8_t b,uint8_t leds_len){
       uint8_t buf[5] = {num,r,g,b,leds_len};
-      dev.Write(DevID,buf,5);
+      dev_.Write(dev_id_,buf,5);
     }
   }
 }
--- Ws2812b_driver.hpp
+++ Ws2812b_driver.hpp
@@ -3,10 +3,10 @@
 namespace CAN_Device_Lib{
   namespace Device{
     class Ws2812b_driver{
-      CAN_Device& dev;
-      uint16_t DevID;
+      CAN_Device& dev_;
+      uint16_t dev_id_;
     public:
-      Ws2812b_driver(CAN_Device& dev,uint16_t id):dev{dev},DevID{id}{}
+      Ws2812b_driver(CAN_Device& dev,uint16_t id):dev_{dev},dev_id_{id}{}
       void Update(uint8_t num,uint8_t r,uint8_t g,uint8_t b,uint8_t leds_len);
     };
   }
