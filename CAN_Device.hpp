3157d170659fc77662a4289d3dd6f654dc05d870
kagaminnnnn <toida2003@gmail.com>
2022-03-14 19:01:56 +0900

    del minor function

--- CAN_Device.hpp
+++ CAN_Device.hpp
@@ -2,9 +2,7 @@
 #define CAN_DEV_H
 
 #include "Device.hpp"
-#include "ytRsSolenoidDriver.hpp"
 #include "MD_Base.hpp"
-#include "ReadOdometer.hpp"
 #include "Lsw.hpp"
 #include "pureToF.hpp"
 #include "Ws2812b_driver.hpp"
